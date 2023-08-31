43dbde16b6379cec26c46348f13aaf7b48cef66d
KingKazmaa <114176897+KingKazmaa@users.noreply.github.com>
2023-08-31 14:59:18 -0500

    Update L2635.js

--- L2635.js
+++ L2635.js
@@ -1,6 +1,11 @@
-const map = (arr, fn) => {
-    for (let i=0;i<arr.length;i++) {
-        arr[i] = fn(arr[i], i)
-    }
-    return arr
-}
+let a = [2,2,3,3,5,0]
+
+
+const removeElement = (nums, val) => {
+        for (let i = 0; i < nums.length; i++) {
+            nums[i] === val ? nums.splice(nums[i], nums[i])
+            : 
+        }
+        return nums
+};
+console.log(removeElement([0,1,2,2,3,0,4,2], 3));
